f9fc47d185718258f69fab5e93591d4e0a7e6589
risanBenichou <ris.benichou@gmail.com>
2023-02-08 14:54:24 +0200

    basic item preview done - minimal design using bs

---
+++ src/cmps/store-item.tsx
@@ -0,0 +1,29 @@
+import { Card } from 'react-bootstrap'
+
+type StoreItemProps = {
+    id: string,
+    name: string,
+    price: number,
+    imgUrl: string
+}
+
+export function StoreItem({ id, name, price, imgUrl }:
+    StoreItemProps) {
+
+    return <Card>
+        <Card.Img
+            variant="top"
+            src={imgUrl}
+            height="200px"
+            style={{ objectFit: "cover" }}
+        />
+        <Card.Body className="d-flex flex-column">
+            <Card.Title className="d-flex 
+            justify-content-space-between 
+            align-items-baseline mb-4">
+                <span className="fs-2">{name}</span>
+                <span className="ms-2">{price}</span>
+            </Card.Title>
+        </Card.Body>
+    </Card>
+}
